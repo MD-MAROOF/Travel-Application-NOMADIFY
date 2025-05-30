1c60bd775059ffbe8b355deca312fd6b7e1ab213
Maroof MD <sayyedmaroof504@gmail.com>
2025-05-29 19:23:56 -0500

    connected pages after signup and logging in

--- middleware.js
+++ middleware.js
@@ -1,8 +1,21 @@
+const { model } = require("mongoose");
+
 module.exports.isLoggedIn = (req, res, next) => {
+
     if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error", "You must be logged in to create listing!");
         return res.redirect("/login");
     }
 
     next();
-} 
+};
+
+module.exports.saveRedirectUrl = (req, res, next) =>{
+    if(req.session.redirectUrl)
+    {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
--- routes/listing.js
+++ routes/listing.js
@@ -34,11 +34,13 @@ router.get("/new", isLoggedIn, (req, res) => {
 //Show Route
 router.get("/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id).populate("reviews");
+  const listing = await Listing.findById(id).populate("reviews").populate("owner");
   if (!listing) {
     req.flash("error", "Your Listing doesn't exist");
     res.redirect("/listings");
   }
+
+  console.log(listing);
   res.render("listings/show.ejs", { listing });
 }));
 
